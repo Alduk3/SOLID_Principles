a31a925ff9432dabd2a94d99a2e4774a8c299d31
Alejandro Duque <alduk0110@gmail.com>
2024-07-03 14:11:58 -0500

    Exercise 3 I - Interface Segregation Principle

---
+++ _03_Worker/src/HumanWorker.java
@@ -0,0 +1,11 @@
+public class HumanWorker implements Workable, Eatable{
+    @Override
+    public void eat() {
+        // Logic to eat
+    }
+
+    @Override
+    public void work() {
+        // Logic to work
+    }
+}
